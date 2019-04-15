5acd372970d2c036970e38c942df91d8dbc5411d
nehavari <vari.neha@gmail.com>
2019-04-15 09:21:37 +0530

    persitetn stack

--- lists/stack.py
+++ lists/stack.py
@@ -1,1 +1,44 @@
+class Node:
 
+    def __init__(self, value, nextt=None):
+        self.__value = value
+        self.__next = nextt
+
+    def setValue(self, value):
+        self.__value = value
+
+    def getValue(self):
+        return self.__value
+
+    def setNextNode(self, nextt):
+        self.__next = nextt
+
+    def getNextNode(self):
+        return self.__next
+
+
+class Stack:
+
+    def __init__(self, node=None):
+        self.__head = node
+
+    def isEmpty(self):
+        return True if self.__head is None else False
+
+    def push(self, e):
+        if not self.__head:
+            self.__head = Node(e)
+        else:
+            node = Node(e)
+            node.setNextNode(self.__head)
+            self.__head = node
+
+    def pop(self):
+        if not self.__head:
+            raise Exception('Stack is empty')
+        return self.__head.getValue(), Stack(self.__head.getNextNode())
+
+    def peek(self):
+        if not self.__head:
+            raise Exception('Stack is empty')
+        return self.__head.getValue()
---
+++ tests/lists/test_stack.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from lists.stack import Stack
+import unittest
+
+
+class TestStack(TestCase):
+
+    def test_head(self):
+        stack = Stack()
+        stack.push('element 1')
+        stack.push('element 2')
+        output = stack.pop()
+        self.assertEqual(output[0], 'element 2')
+        self.assertEqual(stack.peek(), 'element 2')
+        self.assertEqual(output[1].peek(), 'element 1')
+
+    def test_tail(self):
+        stack = Stack()
+        stack.push('element 1')
+        stack.push('element 2')
+        self.assertEqual(stack.peek(), 'element 2')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
