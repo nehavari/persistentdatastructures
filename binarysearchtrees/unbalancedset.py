244ea202961f9ae698fedbfec144a018bdaba17b
nehavari <vari.neha@gmail.com>
2019-05-05 22:45:44 +0530

    Introducing Persistent Unbalanced Set

---
+++ binarysearchtrees/unbalancedset.py
@@ -0,0 +1,165 @@
+from lists.stack import Stack
+from copy import copy
+
+
+class _NodeInorderIterator(object):
+
+    def __init__(self, node):
+        self._node = node
+        self._accumulator = Stack()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._accumulator.isEmpty() and not self._node:
+            raise StopIteration
+
+        if self._node:
+            current = self._node
+            while current:
+                self._accumulator.push(current)
+                current = current.left
+
+        node = self._accumulator.pop()
+        self._node = node.right
+
+        return node
+
+
+class _Node(object):
+
+    __slots__ = ('left', 'value', 'right')
+
+    def __init__(self, value, left=None, right=None):
+        self.left = left
+        self.value = value
+        self.right = right
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+    def __copy__(self):
+        return _Node(self.value, left=self.left, right=self.right)
+
+    def __iter__(self):
+        return _NodeInorderIterator(self)
+
+
+class _UnbalancedSetInorderIterator(object):
+
+    def __init__(self, node):
+        self._node = node
+        self._accumulator = Stack()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._accumulator.isEmpty() and not self._node:
+            raise StopIteration
+
+        if self._node:
+            current = self._node
+            while current:
+                self._accumulator.push(current)
+                current = current.left
+
+        node = self._accumulator.pop()
+        self._node = node.right
+
+        return node.value
+
+
+class UnbalancedSet(object):
+
+    __slots__ = ('__root', )
+
+    def __init__(self, root_val=None):
+        if root_val:
+            object.__setattr__(self, '_UnbalancedSet__root', _Node(root_val))
+        else:
+            object.__setattr__(self, '_UnbalancedSet__root', None)
+
+    def __len__(self):
+        length = 0
+        for node in self.__root:
+            length += 1
+        return length
+
+    def __str__(self):
+        args = '['
+        if len(self) == 0:
+            args += ''
+        elif len(self) == 1:
+            args += str(self.__root.value)
+        else:
+            for node in self.__root:
+                args += str(node.value) + ', '
+
+        args = args[:-2]
+
+        args += ']'
+
+        return args
+
+    def __repr__(self):
+        args = '['
+        if len(self) == 0:
+            args += ''
+        else:
+            for node in self.__root:
+                args += str(node.value) + ', '
+
+        args = args[:-2]
+
+        args += ']'
+
+        return args
+
+    def __eq__(self, other):
+        if len(self) != len(other):
+            return False
+
+        for valueSelf, valueOther in zip(self, other):
+            if valueSelf != valueOther:
+                return False
+        return True
+
+    def __iter__(self):
+        return _UnbalancedSetInorderIterator(self.__root)
+
+    def is_member(self, value):
+        for node in self.__root:
+            if node.value == value:
+                return True
+        return False
+
+    def _insert(self, node, element):
+        if element < node.value:
+            if node.left:
+                cnode = copy(node.left)
+                node.left = cnode
+                self._insert(cnode, element)
+            else:
+                node.left = _Node(element)
+        elif element > node.value:
+            if node.right:
+                cnode = copy(node.right)
+                node.right = cnode
+                self._insert(cnode, element)
+            else:
+                node.right = _Node(element)
+
+    def insert(self, element):
+        if not self.__root:
+            object.__setattr__(self, '_UnbalancedSet__root', _Node(element))
+            return self
+        else:
+            set = UnbalancedSet()
+            object.__setattr__(set, '_UnbalancedSet__root', copy(self.__root))
+            self._insert(set.__root, element)
+            return set
---
+++ tests/binarysearchtrees/test_unbalancedSet.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+from binarysearchtrees.unbalancedset import UnbalancedSet
+import unittest
+
+
+class TestUnbalancedSet(TestCase):
+
+    def test__insert(self):
+        set = UnbalancedSet()
+        set.insert(5)
+        self.assertEqual(set, [5])
+
+        set = set.insert(4)
+        self.assertEqual(set, [4, 5])
+
+        set = set.insert(8)
+        self.assertEqual(set, [4, 5, 8])
+
+        set = set.insert(3)
+        set = set.insert(6)
+        self.assertEqual(set, [3, 4, 5, 6, 8])
+        print(set)
+
+    def test_is_member(self):
+        set = UnbalancedSet().insert(5).insert(4).insert(8).insert(3).insert(6)
+        self.assertTrue(set.is_member(3))
+        self.assertFalse(set.is_member(10))
+
+    def test_insert_for_immutability(self):
+        set = UnbalancedSet(10)
+        set1 = set.insert(2)
+        set2 = set.insert(8)
+        set3 = set2.insert(2)
+
+        self.assertEqual(set, [10])
+        self.assertEqual(set1, [2, 10])
+        self.assertEqual(set2, [8, 10])
+        self.assertEqual(set3, [2, 8, 10])
+
+
+if __name__ == '__main__':
+    unittest.main()
